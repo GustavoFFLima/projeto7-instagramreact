7b5b4bdb3f95472134f2fd5f61e292e3620c79fa
GustavoFFLima <frazao39@gmail.com>
2022-11-19 23:17:17 -0300

    Correções e add class para correções

--- src/Usuario.js
+++ src/Usuario.js
@@ -1,12 +1,12 @@
 export default function Usuario () {
     return (
-        <div class="usuario">
-          <img src="assets/img/catanacomics.svg" />
+        <div class="usuario" data-test="user">
+          <img src="assets/img/catanacomics.svg" data-test="profile-image"/>
           <div class="texto">
             <strong>catanacomics</strong>
-            <span>
+            <span data-test="name">
               Catana
-              <ion-icon name="pencil"></ion-icon>
+              <ion-icon name="pencil" data-test="edit-name"></ion-icon>
             </span>
           </div>
         </div>
