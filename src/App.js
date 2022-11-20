3ef66c46fa70256c85095e1842a485983af4697d
GustavoFFLima <frazao39@gmail.com>
2022-11-19 22:25:29 -0300

    Componentes criados

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./NavBar"
+import Corpo from "./Corpo"
+import FundoMobile from "./FundoMobile";
+
+export default function App(){
+    return(
+        <div>
+            <Navbar />
+            <Corpo />
+            <FundoMobile />
+        </div>
+    );
+};
