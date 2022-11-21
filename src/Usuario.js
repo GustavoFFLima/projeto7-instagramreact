6daa8460dcc4ea4a7836453ebe43bd49282aebee
GustavoFFLima <frazao39@gmail.com>
2022-11-21 15:21:04 -0300

    Usuario, nome e  foto editavel

--- src/Usuario.js
+++ src/Usuario.js
@@ -1,26 +1,39 @@
-export default function Usuario () {
-  const usuario = [{
-    imagem:"assets/img/catanacomics.svg", 
-    texto:"catanacomics", 
-    name:"Catana"
-  }];
+import React from "react";
 
+let usuario = [{
+  imagem:"assets/img/catanacomics.svg", 
+  texto:"catanacomics", 
+  name:"Catana"
+}];
+
+export default function Usuario () {
   return (
-      <div class="usuario" data-test="user">
-        {usuario.map((item) => <UsuarioPagina imagem={item.imagem} texto={item.texto} name={item.name}/>)}
-      </div>
+    <div class="usuario" data-test="user">
+      {usuario.map((item) => <UsuarioPagina imagem={item.imagem} texto={item.texto} name={item.name}/>)}
+    </div>
   )
 }
 
 function UsuarioPagina(props){
+  const [nome, setNome] = React.useState(props.name)
+  const [imagem, setImagem] = React.useState(props.imagem) 
+
+  function editarNome() {
+    setNome(prompt("Qual é o seu nime?"))
+  }
+
+  function editarImagem() {
+    setImagem(prompt("Qual é o seu nime?"))
+  }
+
   return(
     <div>
-      <img src={props.imagem} data-test="profile-image"/>
+      <img onClick={() => editarImagem()}  src={imagem} data-test="profile-image"/>
       <div class="texto">
         <strong>{props.texto}</strong>
         <span data-test="name">
-          {props.name}
-          <ion-icon name="pencil" data-test="edit-name"></ion-icon>
+          {nome}
+          <ion-icon onClick={() => editarNome()} name="pencil" data-test="edit-name"></ion-icon>
         </span>
       </div>
     </div>
